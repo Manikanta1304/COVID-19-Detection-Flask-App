a89e8964007cf44adc3de3f396d6bed7c33c6a56
Manikanta <69647433+Manikanta1304@users.noreply.github.com>
2022-06-30 17:41:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os
 from werkzeug.utils import secure_filename
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
